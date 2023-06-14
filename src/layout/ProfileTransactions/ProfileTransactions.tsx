0b4f1fbd3ecf68a6527737ed7e396f95efada6e1
joshex150 <joshex150@gmail.com>
2023-06-14 12:43:26 +0100

    transaction

--- src/layout/ProfileTransactions/ProfileTransactions.tsx
+++ src/layout/ProfileTransactions/ProfileTransactions.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import useLocalStorage from "../../hook/useLocalStorage";
 import { userContext } from "../../context/User/userContext";
@@ -12,29 +12,33 @@ const ProfileTransactions: React.FC = () => {
   const decryptedData = decryptedBytes.toString(enc.Utf8);
   const flag = decryptedData ? JSON.parse(decryptedData) : {};
   const [invoice, setinvoice] = useState<any>();
-  const fetchInvoices = async () => {
-    try {
-      const response = await fetch("https://api-bank-xi.vercel.app/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: flag?.email.toLowerCase(),
-        }),
-      });
-
-      if (response.ok) {
-        const invoiceRows = await response.json();
-        setinvoice(invoiceRows)
-        console.log(invoice)
-      } else {
-        console.error("Failed to fetch invoices");
+  useEffect(() => {
+   const fetchInvoices = async () => {
+      try {
+        const response = await fetch("https://api-bank-xi.vercel.app/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: flag?.email.toLowerCase(),
+          }),
+        });
+  
+        if (response.ok) {
+          const invoiceRows = await response.json();
+          setinvoice(invoiceRows)
+          console.log(invoice)
+        } else {
+          console.error("Failed to fetch invoices");
+        }
+      } catch (error) {
+        console.error("Failed to fetch invoices", error);
       }
-    } catch (error) {
-      console.error("Failed to fetch invoices", error);
-    }
-  };
+    };
+    fetchInvoices();
+  }, [])
+  
 
   return <div>ProfileTransactions</div>;
 };
